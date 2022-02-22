424ac274c91339d22268406679f7f1fb7a7f31a0
ins4w <amejia.foo@gmail.com>
2022-02-22 14:07:12 -0500

    fix: app requirements

---
+++ src/app.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+# returns an array of words of length n or writes words of length n to a file
+def getWordsOfLength(length, outfile_name = None):
+    page = requests.get("https://github.com/javierarce/palabras/blob/master/listado-general.txt")
+    soup = BeautifulSoup(page.text, 'lxml')
+    words_tags = soup.find_all('td', class_ = 'blob-code')
+    words = list(map(lambda word_tag: word_tag.text, words_tags))
+    if outfile_name != None:
+        with open(outfile_name, 'w') as outfile:
+            for word in words:
+                if len(word) == length:
+                    outfile.write(word + '\n')
+    else:
+        return words
+
+print(getWordsOfLength(1))
