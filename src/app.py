c93ffc91b6dadffdaecbf5e270ee465b1e302ebd
Insaw <insaw@outlook.es>
2022-02-26 16:04:10 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -16,10 +16,8 @@ def getSpanishWords(words_length, total_words = None, outfile_path = None):
     shuffle(words_tags_text)
 
     for word in words_tags_text:
-        
         if len(word) == words_length:
             words.append(word)
-            
         if len(words) == total_words:
             break
 
